5efb276bd10dc1b1b070a4d9ba5581bbe5d81b7b
Álvaro Ferreira <alvaro.ferreira@amcom.com.br>
2022-09-23 18:13:21 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -14,23 +14,24 @@ async function bootstrap() {
     logger: ['error', 'error', 'warn'],
   });
 
-  const whitelist = ['http://localhost:3000', 'https://www.website.com'];
+  app.enableCors()
 
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('allowed cors for:', origin);
-        callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
-  });
+  // const whitelist = ['http://localhost:3000', 'https://facilitaai-api.herokuapp.com'];
+  // app.enableCors({
+  //   origin: function (origin, callback) {
+  //     if (whitelist.indexOf(origin) !== -1) {
+  //       console.log('allowed cors for:', origin);
+  //       callback(null, true);
+  //     } else {
+  //       console.log('blocked cors for:', origin);
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   allowedHeaders:
+  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+  //   credentials: true,
+  // });
 
   app.setGlobalPrefix(API_PREFIX);
 

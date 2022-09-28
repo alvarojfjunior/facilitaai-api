aede5ee120970aec0e2e2149648494fb46c25bfb
Álvaro Ferreira <alvaro.ferreira@amcom.com.br>
2022-09-28 18:49:31 -0300

    feat: start

--- start.bat
+++ start.bat
@@ -3,4 +3,4 @@ call git pull
 call npm i
 call docker-compose up -d --remove-orphans
 call npx prisma migrate dev --name %DATE:~10,4%%DATE:~4,2%%DATE:~7,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
-npm run start:dev
+call npm run start:dev

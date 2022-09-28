138ed64363c3273cdccc6b6a7276021c4cb49241
Álvaro Ferreira <alvaro.ferreira@amcom.com.br>
2022-09-28 11:50:55 -0300

    fix: production cors

--- src/main.ts
+++ src/main.ts
@@ -25,8 +25,20 @@ async function bootstrap() {
   );
 
   if (process.env.NODE_ENV === 'production') {
+    const whitelist = ['http://localhost:3000', 'https://facilitaai.vercel.app'];
     app.enableCors({
-      origin: 'http://localhost:3000',
+      origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+          console.log('allowed cors for:', origin);
+          callback(null, true);
+        } else {
+          console.log('blocked cors for:', origin);
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      allowedHeaders:
+        'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+      methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
       credentials: true,
     });
   } else {
